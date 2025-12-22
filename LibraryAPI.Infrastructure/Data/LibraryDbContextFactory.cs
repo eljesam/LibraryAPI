7570646ebaecb6701fa4231c177ee3dfe07a8798
eljesam <124920260+eljesam@users.noreply.github.com>
2025-12-22 18:19:38 +0000

    implemented dbcontext factory to support entity framework and migrations

---
+++ LibraryAPI.Infrastructure/Data/LibraryDbContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace LibraryAPI.Infrastructure.Data;
+
+public class LibraryDbContextFactory : IDesignTimeDbContextFactory<LibraryDbContext>
+{
+public LibraryDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<LibraryDbContext>();
+        optionsBuilder.UseSqlite("Data Source=library.db");
+
+        return new LibraryDbContext(optionsBuilder.Options);
+    }
+}
