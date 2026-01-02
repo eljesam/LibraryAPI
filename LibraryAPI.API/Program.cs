e3c893649618b474139cf9866a2077bd14858dcf
eljesam <124920260+eljesam@users.noreply.github.com>
2026-01-02 00:03:06 +0000

    Fixed typo

--- LibraryAPI.API/Program.cs
+++ LibraryAPI.API/Program.cs
@@ -52,8 +52,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
-app.UserAuthentication();
-app.UserAuthorization();
+app.UseAuthentication();
+app.UseAuthorization()
 
 
 app.Run();
