08749c9ffbb000c392cc25d6e5cf7e1fdee9f7bc
eljesam <124920260+eljesam@users.noreply.github.com>
2026-01-02 00:01:40 +0000

    Applied authentication

---
+++ LibraryAPI.API/Controllers/AuthController.cs
@@ -0,0 +1,47 @@
+﻿using LibraryAPI.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+[ApiController]
+[Route("api/auth")]
+
+
+public class AuthController:ControllerBase
+{
+    private readonly IConfiguration _config;
+    private readonly IUserRepository _userRepository;
+    
+    public AuthController(IConfiguration config, IUserRepository userRepository)
+    {
+        _config = config;
+        _userRepository = userRepository;
+    }
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(string email)
+    {
+        var user = await _userRepository.GetByEmailAsync(email);
+        if (user == null)
+        {
+            return Unauthorized();
+        }
+
+        var claims = new[]
+        {
+            new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            new Claim(ClaimTypes.Email, user.Email)
+        };
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JwtSettings:Key"]));
+        var token = new JwtSecurityToken(
+            issuer: _config["JwtSettings:Issuer"],
+            audience: _config["JwtSettings:Audience"],
+            claims: claims,
+            expires: DateTime.UtcNow.AddHours(1),
+            signingCredentials: new SigningCredentials(key, SecurityAlgorithms.HmacSha256)
+        );
+
+        return Ok(new { token = new JwtSecurityTokenHandler().WriteToken(token) });
+    }
+}
--- LibraryAPI.API/Program.cs
+++ LibraryAPI.API/Program.cs
@@ -4,10 +4,32 @@ using LibraryAPI.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 using LibraryAPI.Application.Interfaces;
 using LibraryAPI.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+var jwtSettings = builder.Configuration.GetSection("JwtSettings");
+var key = Encoding.ASCII.GetBytes(jwtSettings["Key"]!);
 
 // Add services to the container.
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,,
+            ValidateAudience = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwtSettings["Issuer"],
+            ValidAudience = jwtSettings["Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(key)
+        };
+    });
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -30,6 +52,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
+app.UserAuthentication();
+app.UserAuthorization();
 
 
 app.Run();
